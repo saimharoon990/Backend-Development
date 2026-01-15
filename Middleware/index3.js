53f3cdfc9eeec9e46aaeb689a7162c2022040713
saimharoon990 <saimharoon990@gmail.com>
2026-01-15 17:22:24 +0500

    MiddleWare files added

--- Middleware/index3.js
+++ Middleware/index3.js
@@ -1,7 +1,14 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 const port = 3000;
+
+function logger(req, res, next) {
+  console.log("Request Method: ", req.method);
+  console.log("Request URL: ", req.url);
+  next()
+}
 
 app.use(logger);
 
